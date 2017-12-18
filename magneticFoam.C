0a86b20b2dfa7e4d6e76a093505924a749ba3d0d
zuozhuwu <zuozhuwu@gmail.com>
2017-12-18 16:29:42 +0800

    modified a lot

--- magneticFoam.C
+++ magneticFoam.C
@@ -44,23 +44,6 @@ Description
 
 int main(int argc, char *argv[])
 {
-    argList::addBoolOption
-    (
-        "noH",
-        "do not write the magnetic field intensity field"
-    );
-
-    argList::addBoolOption
-    (
-        "noB",
-        "do not write the magnetic flux density field"
-    );
-
-    argList::addBoolOption
-    (
-        "HdotGradH",
-        "write the paramagnetic particle force field"
-    );
 
     #include "setRootCase.H"
     #include "createTime.H"
@@ -83,69 +66,9 @@ int main(int argc, char *argv[])
 
     psi.write();
 
-    if (!args.optionFound("noH") || args.optionFound("HdotGradH"))
-    {
-        volVectorField H
-        (
-            IOobject
-            (
-                "H",
-                runTime.timeName(),
-                mesh
-            ),
-            fvc::reconstruct(fvc::snGrad(psi)*mesh.magSf())
-        );
+    #include "calcBH.H"
 
-        if (!args.optionFound("noH"))
-        {
-            Info<< nl
-                << "Creating field H for time "
-                << runTime.timeName() << endl;
-
-            H.write();
-        }
-
-        if (args.optionFound("HdotGradH"))
-        {
-            Info<< nl
-                << "Creating field HdotGradH for time "
-                << runTime.timeName() << endl;
-
-            volVectorField HdotGradH
-            (
-                IOobject
-                (
-                    "HdotGradH",
-                    runTime.timeName(),
-                    mesh
-                ),
-                H & fvc::grad(H)
-            );
-
-            HdotGradH.write();
-        }
-    }
-
-    if (!args.optionFound("noB"))
-    {
-        Info<< nl
-            << "Creating field B for time "
-            << runTime.timeName() << endl;
-
-        volVectorField B
-        (
-            IOobject
-            (
-                "B",
-                runTime.timeName(),
-                mesh
-            ),
-            constant::electromagnetic::mu0
-           *fvc::reconstruct(murf*fvc::snGrad(psi)*mesh.magSf() + murf*Mrf)
-        );
-
-        B.write();
-    }	
+    #include "calcInductance.H"
 
     Info<< "\nEnd\n" << endl;
 
